0d32d762daaf1b820de99e0be9d70cdaef86820b
Rebecca Marusca <maruscarebecca@gmail.com>
2026-09-03 13:23:32 +0300

    fix fix fix

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,6 @@ from firebase_admin import credentials, auth as fb_auth, firestore
 from fastapi import FastAPI, Header, HTTPException, Depends
 from datetime import datetime, timezone
 from routers import homes, nodes, events, users
-
-cred = credentials.Certificate('serviceAccountKey.json')
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = FastAPI()
